6a89151ed315135e4660eaa9be155c3ec10385f2
Quintin Hitlon <qhilton@byu.edu>
2025-02-22 09:29:57 -0700

    fix GameDAO file

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -2,30 +2,31 @@ package dataaccess;
 
 import chess.ChessGame;
 import model.GameData;
-import model.UserData;
+import java.util.Collection;
+import java.util.Map;
 
 public class GameDAO implements DataAccess {
-    GameData gameData;
-
-    //@Override
-    //public void createUser(String username, String password, String email) {}
+    Map<Integer, GameData> allGames;
 
     public void createGame(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
-        gameData = new GameData(gameID, whiteUsername, blackUsername, gameName, game);
+        allGames.put(gameID, new GameData(gameID, whiteUsername, blackUsername, gameName, game));
+    }
+
+    public GameData getGame(int gameID) {
+        return allGames.get(gameID);
+    }
+
+    public Collection<GameData> listGames() {
+        return allGames.values();
+    }
+
+    public void updateGame(int gameID, ChessGame game) {
+        GameData gameData = allGames.get(gameID);
+        allGames.put(gameID, new GameData(gameID, gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName(), game));
     }
 
     @Override
     public void clear() {
-        gameData = null;
-    }
-
-    //@Override
-//    public UserData getUser() {
-//        return null;
-//    }
-
-    //@Override
-    public GameData getGame() {
-        return gameData;
+        allGames = null;
     }
 }

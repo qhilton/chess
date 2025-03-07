17c164ac9560b2847e2a77872d4cb5638bb75f4d
Quintin Hitlon <qhilton@byu.edu>
2025-03-06 20:59:06 -0700

    remove comments

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -2,9 +2,7 @@ package dataaccess;
 
 import model.UserData;
 import org.mindrot.jbcrypt.BCrypt;
-//import exception.ResponseException;
 import java.sql.*;
-import static java.sql.Statement.RETURN_GENERATED_KEYS;
 
 public class SQLUserDAO {
 
@@ -72,33 +70,9 @@ public class SQLUserDAO {
                 preparedStatement.executeUpdate();
             }
         } catch (SQLException | DataAccessException e) {
-            //throw new DataAccessException(e);
+            //throw new DataAccessException("" + e);
         }
     }
-
-
-
-//    private int executeUpdate(String statement, Object... params) throws ResponseException {
-//        try (var conn = DatabaseManager.getConnection()) {
-//            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
-//                for (var i = 0; i < params.length; i++) {
-//                    var param = params[i];
-//                    if (param instanceof String p) ps.setString(i + 1, p);
-//                    else if (param == null) ps.setNull(i + 1, NULL);
-//                }
-//                ps.executeUpdate();
-//
-//                var rs = ps.getGeneratedKeys();
-//                if (rs.next()) {
-//                    return rs.getInt(1);
-//                }
-//
-//                return 0;
-//            }
-//        } catch (SQLException e) {
-//            throw new ResponseException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
-//        }
-//    }
 
     private final String[] createStatements = {
             """
@@ -110,18 +84,4 @@ public class SQLUserDAO {
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """
     };
-
-
-//    private void configureDatabase() throws ResponseException {
-//        DatabaseManager.createDatabase();
-//        try (var conn = DatabaseManager.getConnection()) {
-//            for (var statement : createStatements) {
-//                try (var preparedStatement = conn.prepareStatement(statement)) {
-//                    preparedStatement.executeUpdate();
-//                }
-//            }
-//        } catch (SQLException ex) {
-//            throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
-//        }
-//    }
 }
